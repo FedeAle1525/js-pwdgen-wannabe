9828d43339025fb712615fbc7558db81df655bcc
FedeAle1525 <fedeale.1525@gmail.com>
2023-01-30 14:55:40 +0100

    FIRST COMMIT

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 1. Chiedere Nome all'Utente
+// 2. Chiedere Cognome all'Utente
+// 3. Chiedere Colore Preferito all'Utente
+// 4. Chiedere Numero all'Utente
+// 5. Generare Password concantenando i dati richiesti
+// 6. Stampare Password su file HTML
